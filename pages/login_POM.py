90b6eeab7c2b725b36272a04942eecdb56c73abd
Lukasz Rzadzki <lukasz.rzadzki@globallogic.com>
2025-05-15 00:12:55 +0200

    POM implementation started

---
+++ pages/login_POM.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.ui import Select
+
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.username_field = (By.ID, "user-name")
+        self.password_field = (By.ID, "password")
+        self.login_button = (By.ID, "login-button")
+    
+    def enter_username(self, username):
+        self.driver.find_element(*self.username_field).send_keys(username)
+    
+    def enter_password(self, password):
+        self.driver.find_element(*self.password_field).send_keys(password)
+
+    def click_logn(self):
+        self.driver.find_element(*self.login_button).click()
+        
